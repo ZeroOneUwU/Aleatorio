015d9dbdbb01c6e5c1aa6b5a5e47bb1a944ce1e2
eryka <eryka@LAPTOP-9IBP64BJ>
2022-05-30 19:39:21 -0500

    Agregar archivos de proyecto.

---
+++ Aleatorio/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Aleatorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonoff = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.picture3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture1
+            // 
+            this.picture1.BackColor = System.Drawing.SystemColors.Control;
+            this.picture1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picture1.Location = new System.Drawing.Point(59, 61);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(124, 122);
+            this.picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture1.TabIndex = 2;
+            this.picture1.TabStop = false;
+            this.picture1.Click += new System.EventHandler(this.pictureFruta_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Aleatorio.Properties.Resources.palanca2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(567, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 159);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // buttonoff
+            // 
+            this.buttonoff.BackgroundImage = global::Aleatorio.Properties.Resources.ONOFF;
+            this.buttonoff.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonoff.Location = new System.Drawing.Point(638, 208);
+            this.buttonoff.Name = "buttonoff";
+            this.buttonoff.Size = new System.Drawing.Size(38, 32);
+            this.buttonoff.TabIndex = 0;
+            this.buttonoff.UseVisualStyleBackColor = true;
+            this.buttonoff.Click += new System.EventHandler(this.buttonoff_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Aleatorio.Properties.Resources.palanca2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(567, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(81, 83);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picture2
+            // 
+            this.picture2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picture2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picture2.Location = new System.Drawing.Point(205, 61);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(125, 122);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture2.TabIndex = 5;
+            this.picture2.TabStop = false;
+            // 
+            // picture3
+            // 
+            this.picture3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picture3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picture3.Location = new System.Drawing.Point(351, 61);
+            this.picture3.Name = "picture3";
+            this.picture3.Size = new System.Drawing.Size(119, 122);
+            this.picture3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture3.TabIndex = 6;
+            this.picture3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = global::Aleatorio.Properties.Resources.aleatorio;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(688, 252);
+            this.ControlBox = false;
+            this.Controls.Add(this.picture3);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonoff);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonoff;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.PictureBox picture3;
+    }
+}
+
---
+++ Aleatorio/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aleatorio
+{
+ 
+   public partial class Form1 : Form
+    {
+        //Declaracion de variables globales
+        int varaleatorio = 0;
+        int varaleatorio2 = 0;
+        int varaleatorio3 = 0;
+
+        int i=0, j=0, k=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonoff_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = BackColor;
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void pictureFruta_Click(object sender, EventArgs e)
+        {
+           
+           
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox1.Visible = false;
+
+            Random aleat = new Random();
+            Random aleat2 = new Random();
+            Random aleat3 = new Random();//declaramos el objecto randoms
+            varaleatorio = aleat.Next(15)+1;
+            varaleatorio2 = aleat2.Next(15)-3;
+            varaleatorio3 = aleat3.Next(15)+5;//asignamos el valor de un rango de 0-2
+
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+
+            switch (i)
+            {
+                case 0:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Arandono;
+                   
+
+                    break;
+                case 1:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Bar;
+                   
+
+                    break;
+                case 2:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Bar2;
+                    
+                    break;
+
+                case 4:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Bar3;
+                    
+
+                    break;
+                case 5:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Campana;
+                    
+
+                    break;
+                case 6:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Cereza;
+                   
+                    break;
+                case 7:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Diamante;
+                   
+
+                    break;
+                case 8:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Dinero;
+
+                    break;
+
+
+                case 9:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Herradura;
+
+
+                    break;
+                case 10:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Limon;
+
+
+                    break;
+                case 11:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Naranja;
+
+                    break;
+
+                case 12:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Sandia;
+
+
+                    break;
+                case 13:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.siete;
+
+
+                    break;
+                case 14:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Toronja;
+
+                    break;
+                case 15:
+                    picture1.Image =
+                        Aleatorio.Properties.Resources.Trevol;
+                 break;
+ 
+            }
+
+            switch (j)
+            {
+                case 0:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Arandono;
+
+
+                    break;
+                case 1:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Bar;
+
+
+                    break;
+                case 2:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Bar2;
+
+                    break;
+
+                case 4:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Bar3;
+
+
+                    break;
+                case 5:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Campana;
+
+
+                    break;
+                case 6:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Cereza;
+
+                    break;
+                case 7:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Diamante;
+
+
+                    break;
+                case 8:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Dinero;
+
+                    break;
+
+
+                case 9:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Herradura;
+
+
+                    break;
+                case 10:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Limon;
+
+
+                    break;
+                case 11:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Naranja;
+
+                    break;
+
+                case 12:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Sandia;
+
+
+                    break;
+                case 13:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.siete;
+
+
+                    break;
+                case 14:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Toronja;
+
+                    break;
+                case 15:
+                    picture2.Image =
+                        Aleatorio.Properties.Resources.Trevol;
+                    break;
+
+            }
+
+            switch (k)
+            {
+                case 0:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Arandono;
+
+
+                    break;
+                case 1:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Bar;
+
+
+                    break;
+                case 2:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Bar2;
+
+                    break;
+
+                case 4:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Bar3;
+
+
+                    break;
+                case 5:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Campana;
+
+
+                    break;
+                case 6:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Cereza;
+
+                    break;
+                case 7:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Diamante;
+
+
+                    break;
+                case 8:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Dinero;
+
+                    break;
+
+
+                case 9:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Herradura;
+
+
+                    break;
+                case 10:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Limon;
+
+
+                    break;
+                case 11:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Naranja;
+
+                    break;
+
+                case 12:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Sandia;
+
+
+                    break;
+                case 13:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.siete;
+
+
+                    break;
+                case 14:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Toronja;
+
+                    break;
+                case 15:
+                    picture3.Image =
+                        Aleatorio.Properties.Resources.Trevol;
+                    break;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+             
+           
+           
+            if (j <= varaleatorio2)
+                j++;
+            else
+                j = 0;
+            if (j == varaleatorio2)
+                timer2.Stop();
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (k <= varaleatorio3)
+                k++;
+            else
+                k = 0;
+            if (k == varaleatorio3)
+                timer3.Stop();
+           
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i <= varaleatorio)
+                i++;
+            else
+                i = 0;
+            if(i== varaleatorio)
+                timer1.Stop();
+
+
+            //genera otro nuevo nmero aleatoriotextBox1.Text=textBox1.Text
+            //+i.ToString() + Environment.NewLine;
+            //pero para que fruta desea mostrar
+
+        }
+    }
+}
